5bb1815ff5ab30b5edb77fb26347e4389528a160
Anton Donev <anton.donev@softwaregroup-bg.com>
2021-06-09 16:29:23 +0300

    feat: adding and removing table rows

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
 import Joi from 'joi';
-
-import { Card, InputText, DataTable, Column, Dropdown, InputMask, InputNumber, Calendar } from '../prime';
+import { v4 as uuidv4 } from 'uuid';
+import { Card, InputText, DataTable, Column, Toolbar, Button, Dropdown, InputMask, InputNumber, Calendar } from '../prime';
 import { Styled, StyledType } from './Editor.types';
 import useForm from '../hooks/useForm';
 import Controller from '../Controller';
@@ -25,6 +25,7 @@ const Table = React.forwardRef<{}, any>(({onChange, columns, value, dataKey = 'i
     if (typeof ref === 'function') ref(React.useState({})[0]);
     const cellEditor = React.useCallback((props, field) => <InputText
         type="text"
+        autoFocus={props.index === 1}
         value={props.rowData[field]}
         onChange={({target: {value}}) => {
             const updatedValue = [...props.value];
@@ -44,26 +45,56 @@ const Table = React.forwardRef<{}, any>(({onChange, columns, value, dataKey = 'i
         restored[original.index] = original.value;
         onChange(restored);
     }, [value, onChange]);
-
+    const addNewRow = () => {
+        const id = uuidv4();
+        const newValue = Object.keys(value[0] || {}).reduce((item, key) => ({...item, [key]: '', id: id}), {});
+        const updatedValue = [...value, newValue];
+        onChange(updatedValue);
+        setEditingRows({[id]: true});
+    };
+    const deleteRow = () => {
+        onChange(value.filter(rowData => !selected.includes(rowData)));
+    };
+    const [selected, setSelected] = React.useState([]);
+    const [editingRows, setEditingRows] = React.useState({});
+    const onRowEditChange = (event) => {
+        setEditingRows(event.data);
+    };
+    const leftToolbarTemplate = () => {
+        return (
+            <React.Fragment>
+                <Button label="New" icon="pi pi-plus" className="p-button p-mr-2" onClick={addNewRow} />
+                <Button label="Delete" icon="pi pi-trash" className="p-button" onClick={deleteRow} disabled={!selected || !selected.length} />
+            </React.Fragment>
+        );
+    };
     return (
-        <DataTable
-            value={value}
-            editMode="row"
-            dataKey={dataKey}
-            className="editable-cells-table"
-            onRowEditInit={init}
-            onRowEditCancel={cancel}
-        >
-            {
-                (columns || []).map(({ field, header }) => <Column
-                    key={field}
-                    field={field}
-                    header={header}
-                    editor={props => cellEditor(props, field)}
-                />)
-            }
-            <Column rowEditor headerStyle={{ width: '7rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
-        </DataTable>
+        <>
+            <Toolbar className="p-mb-4" left={leftToolbarTemplate} right={null}></Toolbar>
+            <DataTable
+                value={value}
+                editMode="row"
+                dataKey={dataKey}
+                className="editable-cells-table"
+                onRowEditInit={init}
+                onRowEditCancel={cancel}
+                selection={selected}
+                onSelectionChange={(e) => { setSelected(e.value); }}
+                editingRows={editingRows}
+                onRowEditChange={onRowEditChange}
+            >
+                <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+                {
+                    (columns || []).map(({ field, header }) => <Column
+                        key={field}
+                        field={field}
+                        header={header}
+                        editor={props => cellEditor(props, field)}
+                    />)
+                }
+                <Column rowEditor headerStyle={{ width: '7rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
+            </DataTable>
+        </>
     );
 });
 
@@ -80,7 +111,7 @@ function element(field: { onChange: (...event: any[]) => void; onBlur: () => voi
 
 const Editor: StyledType = ({ classes, className, fields, cards, onSubmit, trigger, get, ...rest }) => {
     const schema = Object.entries(fields).reduce(
-        (schema, [name, {title, validation = Joi.string().allow('')}]) => schema.append({[name]: validation.label(title)}),
+        (schema, [name, {title, validation = Joi.string().min(0).allow('', null).default('label')}]) => schema.append({[name]: validation.label(title || name)}),
         Joi.object()
     );
     const {handleSubmit, reset, control, formState: {errors}} = useForm({resolver: joiResolver(schema)});
@@ -101,10 +132,10 @@ const Editor: StyledType = ({ classes, className, fields, cards, onSubmit, trigg
                         <Card title={title} key={id} className='p-fluid'>
                             {names.map(name =>
                                 <div className="p-field p-grid" key={name}>
-                                    <label className="p-col-12 p-md-4">
+                                    {fields[name].title ? <label className={clsx(fields[name].title ? `p-col-12 ${className || 'p-md-4'}` : '')}>
                                         {fields[name].title}
-                                    </label>
-                                    <div className="p-col-12 p-md-8">
+                                    </label> : null}
+                                    <div className={clsx(fields[name].title ? `p-col-12 ${className || 'p-md-8'}` : 'p-col-12')}>
                                         <Controller
                                             control={control}
                                             name={name}
