7012d72a2a7dd8ce5b231ea2606600760884457b
kristiyan.todorov <kristiyan.todorov@softwaregroup-bg.com>
2022-01-28 14:31:28 +0200

    fix: table column password inputs

--- src/components/lib/column.tsx
+++ src/components/lib/column.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Checkbox, DropdownTest, SelectButton, Calendar, InputMask, InputText, InputTextarea, InputNumber } from '../prime';
+import { Checkbox, DropdownTest, SelectButton, Calendar, InputMask, InputText, InputTextarea, InputNumber, Password } from '../prime';
 import { Property } from '../types';
 import titleCase from './titleCase';
 import getType from './getType';
@@ -118,6 +118,13 @@ export default function columnProps({
                 return new Date(value).toLocaleString();
             };
             break;
+        case 'password': {
+            body = function body(rowData) {
+                return rowData[fieldName]
+                    ? '*'.repeat(10)
+                    : ''
+            }
+        }
     }
     if (!property?.readOnly && editable) {
         editor = function editor(p) {
@@ -217,6 +224,13 @@ export default function columnProps({
                         {...props}
                         name={inputId}
                     />;
+                case 'password':
+                    return <Password
+                        value={p.rowData[fieldName]}
+                        onInput={event => p.editorCallback(event.currentTarget.value)}
+                        id={inputId}
+                        {...props}
+                    />
                 case 'text':
                     return <InputTextarea
                         className='w-full'
